f92005be5e06f622e5efdaa6469748577762975f
Federico Gamboa <federico.gamboa@neitek.com>
2018-03-13 19:07:43 -0600

    first commit

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Neitek
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(Operaciones.Sumar(2, 3));
+        System.out.println(Operaciones.Sumar(1.5, 3.0));
+    }
+}
---
+++ src/calculadora/Operaciones.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculadora;
+
+/**
+ *
+ * @author Neitek
+ */
+public class Operaciones {
+    public static int Sumar(int a, int b){
+        return a+b;
+    }
+    public static float Sumar(float a, float b){
+        return a+b;
+    }
+    public static double Sumar(double a, double b){
+        return a+b;
+    }
+    public static long Sumar(long a, long b){
+        return a+b;
+    }
+}
